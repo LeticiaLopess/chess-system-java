5f9e7ae0458d3a812ea6ec81196f0a0d438ebb10
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-01-12 17:42:00 -0300

    adding class Piece and Board

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,23 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position; // #position: Position -> tem uma posição protegida, não é pra essa posição ser visível na camada de xadrez, por isso é protected
+	private Board board;
+	
+	public Piece(Board board) { // passamos apenas o tabuleiro ao criar a peça pois a posição recém criada será inicialmente como nula
+		super();
+		this.board = board;
+		/*position = null;*/ // se eu não colocasse aqui, o java já iria atribuir nul
+	}
+
+	protected Board getBoard() { // somente classes dentro do mesmo pacote e subclasses vão poder acessar o tabuleiro de uma peça
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	
+	
+}
