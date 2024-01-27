9d679629e837a4f732bd008aedb0cfa7c1931793
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-01-26 21:52:37 -0300

    color yellow and white for the pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,8 +31,21 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
+	
+	// na pasta bin do meu projeto é onde ficam os arquivos compilados do meu projeto (class) 
+	// pra rodar no git bash: java application/Program (classe principal que roda o projeto)
 }
