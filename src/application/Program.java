c9e69539b9ae7c624472ec4d9ef62f66c21885ee
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-01-29 14:40:09 -0300

    cleaning log

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import boardgame.Board;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -15,16 +17,30 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch(); // crio o tabuleiro, inicio a partida
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPieces()); // UI -> User Interface // aqui formo o tabuleiro visualmente
-			System.out.println(); // pulo uma linha
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
 			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
+			try {
+				
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces()); // UI -> User Interface // aqui formo o tabuleiro visualmente
+				System.out.println(); // pulo uma linha
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target); // o source é a peça que está no tabuleiro que você escolhe pra se mover, target é pra onde você quer jogar ela
 			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target); // o source é a peça que está no tabuleiro que você escolhe pra se mover, target é pra onde você quer jogar ela
+			} catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				
+			} catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			
 		}
 	}
 
